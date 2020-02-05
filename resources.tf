065f19db3dbc24a0ae1d6749a6d81f1519e1a137
Sara Taha <st.elzayat@gmail.com>
2020-02-05 09:12:49 +0200

    add aws security group

--- resources.tf
+++ resources.tf
@@ -23,3 +23,17 @@ resource "aws_subnet" "subnet2" {
   vpc_id = "${aws_vpc.environment-example-two.id}"
   availability_zone = "us-west-2b"
 }
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+}
