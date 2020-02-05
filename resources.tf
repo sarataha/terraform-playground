46181cd188d08dc7dffdc132ab1f5f0456afdd9d
Sara Taha <st.elzayat@gmail.com>
2020-02-05 09:09:50 +0200

    add a second aws subnet

--- resources.tf
+++ resources.tf
@@ -17,3 +17,9 @@ resource "aws_subnet" "subnet1" {
   vpc_id = "${aws_vpc.environment-example-two.id}"
   availability_zone = "us-west-2a"
 }
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-2b"
+}
